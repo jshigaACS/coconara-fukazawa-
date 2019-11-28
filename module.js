b426e00a8d240c294224b3f1b9eb8278f9f3831e
root <root@acs30.localdomain>
2019-11-28 13:14:46 +0900

    20191128_front_module

--- module.js
+++ module.js
@@ -1,20 +1,54 @@
-//globalForAjax
-file_data;
+//global ForAjax
+var file_data;
+var file_name;//handleFileSelectの際にファイルnameを取得する
+var res_data;
 
-function connect_ajax(data){
-    var a = data;
+//aタグ押下時の処理
+function handleDownload(){
+    var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+    var content = res_data;
+    var blob = new Blob([ bom, content ], { "type" : "text/csv" });
+    
+    var a_tag = document.getElementById("download");
+        
+    if (window.navigator.msSaveBlob) { 
+        window.navigator.msSaveBlob(blob, "test.csv"); 
+
+        // msSaveOrOpenBlobの場合はファイルを保存せずに開ける
+        window.navigator.msSaveOrOpenBlob(blob, "test.csv"); 
+    } else {
+        a_tag.href = window.URL.createObjectURL(blob);
+    }
 
 }
 
+//ajax成功時にaタグを作る
+function add_btn() {
+    var a_tabg_div = document.getElementById("add_link");
+    var a = document.createElement("a");
+    a.href = "#";
+    a.id = "download";
+    a.download = "test.csv";
+    a.innerText = "ファイルをダウンロードします";
+    a_tabg_div.appendChild(a);
+
+    //click-event
+    handleDownload();
+
+    a.click();
+
+
+}
+
+
+//ファイルの内容取得
 function handleFileSelect(evt) {
     var file = evt.target.files[0];
-
     var reader = new FileReader();
 
     reader.onerror = function(){
      alert('ファイルの読み込みに失敗しました');
     }
-
     reader.onload = function(){
         var csv = reader.result.replace(/\r?\n/g, "");
         var csvArray = csv.split(',');
@@ -26,16 +60,14 @@ function handleFileSelect(evt) {
         }else{
             alert('5000件以下にしてください');
         }
-
     }
     reader.readAsText(file);
 
 }
 
+//ファイルアップボタン押下時
 $(document).ready(function () {
     var file_up = document.getElementById('upc_file_up');
-    //console.log('aaaa')
-    //console.log(file_up)
     var res = [];
     if (window.File && window.FileReader && window.FileList && window.Blob) {
         // Great success! All the File APIs are supported.
@@ -53,3 +85,24 @@ $(document).ready(function () {
 
 });
 
+//submit botton押下時
+$(function(){
+    $('#ajax').on('click',function(){
+        $.ajax({
+            url:'http://localhost:80/phpinfo.php',
+            type:'POST',
+            data:{
+                'fill_list':file_data
+            }
+        })
+        //疎通成功
+        .done((data) =>{
+            res_data = data;
+            console.log(res_data)
+            add_btn();  
+        })
+        .fail((data) =>{
+            console.log('失敗だよ');
+        })
+    });
+});
